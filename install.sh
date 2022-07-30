7060925d48818265b44f30ea62d92a3f6b042f12
Nick Matthews <matthews3102000@gmail.com>
2022-07-30 07:53:19 -0400

    I forgot I had a coffee
    There has been a cup of coffee sat on my desk for the past 4 hours. I forgot that I had it. I have been so focused and stressing over creating this and making sure it is ready for my video.
    
    Sponsor me here on GitHub.

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ sudo add-apt-repository universe -y
 
 sudo apt install cinnamon-desktop-environment lightdm
 
-unzip Windows-XP-Desktops.zip && unzip Windows-XP-Icons.zip
+unzip Windows-XP-Desktops.zip && unzip Windows-XP-3.1.zip
 cd
 cd /home/xp
 mkdir .icons
 mkdir .themes
 mv /home/xp/Desktop/UbuntuXP/Windows-XP-Desktops /home/xp/.themes
-mv /home/xp/Desktop/UbuntuXP/Windows-XP-Icons /home/xp/.icons
+mv /home/xp/Desktop/UbuntuXP/Windows-XP-3.1 /home/xp/.icons
 
