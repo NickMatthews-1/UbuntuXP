db571bdb7b3d24fa7e246815ea5b8f1817dca89d
Nick Matthews <nick@linuxpanic.com>
2023-02-21 00:41:23 +0000

    Yes it really took me 7 months to fix this.
    It actually took me 7 months to fix the glaring mistake I saw. I only saw it after making more install scripts.

--- install.sh
+++ install.sh
@@ -6,9 +6,8 @@ sudo apt install cinnamon-desktop-environment lightdm
 
 unzip Windows-XP-Desktops.zip && unzip Windows-XP-3.1.zip
 cd
-cd /home/xp
+cd /$HOME
 mkdir .icons
 mkdir .themes
-mv /home/xp/Desktop/UbuntuXP/Windows-XP-Desktops /home/xp/.themes
-mv /home/xp/Desktop/UbuntuXP/Windows-XP-3.1 /home/xp/.icons
-
+mv /$HOME/Desktop/UbuntuXP/Windows-XP-Desktops /$HOME/.themes
+mv /$HOME/Desktop/UbuntuXP/Windows-XP-3.1 /$HOME/.icons
